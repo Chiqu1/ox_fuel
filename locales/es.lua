aaab7f968043f3b13d274c54a179c130dd623324
Chiqui <90169400+Chiqu1@users.noreply.github.com>
2022-05-25 23:57:51 -0300

    Spanish ox_fuel translation.
    Spanish locale.

---
+++ locales/es.lua
@@ -0,0 +1,18 @@
+{
+	"tank_full": "El tanque de este vehiculo esta lleno",
+	"petrolcan_cannot_afford": "No tienes suficiente dinero para un bidon de gasolina",
+	"refuel_cannot_afford": "No tienes suficiente dinero para rellenar tu vehiculo",
+	"vehicle_far": "Tu vehiculo esta muy lejos",
+	"pump_fuel_with_can": "Guarde el bidon antes de rellenar",
+	"fuel_help": "Presiona ~INPUT_C2939D45~ para recargar gasolina",
+	"petrolcan_help": "Presiona ~INPUT_C2939D45~ para rellenar o comprar un bidon de gasolina",
+	"leave_vehicle": "Abandona el vehiculo para rellenarlo",
+	"start_fueling": "Rellenar vehiculo",
+	"petrolcan_buy_or_refill": "Comprar o recargar un bidon de gasolina",
+	"fuel_station_blip": "Gasolinera",
+	"not_enough_money": "No tenes suficiente dinero. Te faltan $%s",
+	"fuel_success": "Rellenado a %s - $%s",
+	"petrolcan_refill": "Has pagado $%s por rellenar tu bidon de gasolina",
+	"petrolcan_buy": "Has pagado $%s por un bidon de gasolina",
+	"petrolcan_cannot_carry": "No puedes cargar con este bidon de gasolina."
+}
